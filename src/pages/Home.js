97a319a04f7a1b03223e8b365fa6ed3c425e336a
KangWookLim <lakumwa99@gmail.com>
2022-09-13 21:42:22 +0900

    add Home,Footer,reactstrap

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,13 +1,18 @@
 	import styled from 'styled-components';
     import React from "react";
     import { useNavigate } from "react-router-dom";
+    import Header from "./component/Header"
+    import Footer from "./component/Footer"
 
 function Home() {
     const navigate = useNavigate()
   return (
+    <Header />,
     <div>
-        test text
-    </div>
+        test 
+    </div>,
+    <Footer />
+
   );
 }
 
---
+++ src/pages/component/Header.js
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+import React from "react";
+import { useNavigate } from "react-router-dom";
+
+function Header() {
+const navigate = useNavigate()
+return (
+<div>
+    test text
+</div>
+);
+}
+
+export default Header;
