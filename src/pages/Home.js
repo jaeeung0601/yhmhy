0ac5e4b106d93ded5c31d440bc16a9fd8f144b0c
KangWookLim <lakumwa99@gmail.com>
2022-09-16 20:15:35 +0900

    home 추가

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,20 +1,53 @@
 	import styled from 'styled-components';
     import React from "react";
     import { useNavigate } from "react-router-dom";
-    import Header from "./component/Header"
-    import Footer from "./component/Footer"
+    import Header from "./component/Header";
+    import HomeBgImg from "./assets/MainBg.jpg";
 
 function Home() {
     const navigate = useNavigate()
   return (
-    <MainBG>,
-    <Header />,
-    <div>
-        test 
-    </div>,
-    <Footer />
+    <MainBG>
+      <Header />,
+        <MainWrapper>
+          <h3>
+          니집내집
+          </h3>
+          <div>
+            당신을 위한 인테리어 플랫폼
+          </div>
+        </MainWrapper>
+    </MainBG>
 
   );
 }
 
 export default Home;
+
+
+
+const MainBG = styled.div`
+  height: 100vh;
+  width: 100vw;
+  background-image: url(${HomeBgImg});
+  background-repeat: no-repeat;
+  background-size: cover;
+  object-fit: cover;
+  display: grid;
+  place-items: center;
+`
+
+const MainWrapper = styled.div`
+  position: absolute;
+  min-height: 50vh;
+  min-width: 100vw;
+  display: grid;
+  place-items: center;
+  background-color: rgba(255, 255, 255, 0.8);
+  font-size: 2rem;
+  margin: 0, 1rem;
+  `
+  const h3 = styled.div`
+  font-size: 3rem;
+  margin: 0, 1rem;
+`
--- src/pages/component/Header.js
+++ src/pages/component/Header.js
@@ -5,10 +5,18 @@ import { useNavigate } from "react-router-dom";
 function Header() {
 const navigate = useNavigate()
 return (
-<div>
-    test text
-</div>
+    <Button> Logo</Button>
 );
 }
 
 export default Header;
+
+const Button = styled.button`
+  background: palevioletred;
+  font-size: 2em;
+  border: 2px solid palevioletred;
+  color: white;
+  margin: 0.2em;
+  padding: 0.25em 1em;
+  position: absolute; left: 30px; top: 30px;
+  `
