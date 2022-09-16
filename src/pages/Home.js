0de0fd4a95d67997de6639569e2906874eff63f0
KangWookLim <lakumwa99@gmail.com>
2022-09-16 21:40:26 +0900

    style.css 수정

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -12,21 +12,19 @@ function Home() {
           <h3>
           니집내집
           </h3>
-          <ul class="multi-layout">
-            <li>
+            <div class="MainText">
+                당신을 위한 인테리어 플랫폼
+            </div>
+            <div>
               <button type="button" onclick="location.href='/'">
-                인테리어 테스트
+                테스트 시작하기
               </button>
+            </div>
+            <div>
               <button type="button" onclick="location.href='/'">
                 글 작성
               </button>
-            </li>
-            <li>
-                <div>
-                당신을 위한 인테리어 플랫폼
-                </div>
-            </li>
-          </ul>
+            </div>
         </p>
     </p>
 
