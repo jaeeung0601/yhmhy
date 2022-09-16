1d0a5248e12bfc709a3b7e6da6da3359eaa0ce18
KangWookLim <lakumwa99@gmail.com>
2022-09-16 21:22:09 +0900

    add button and text

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,23 +1,34 @@
-	import styled from 'styled-components';
     import React from "react";
     import { useNavigate } from "react-router-dom";
     import Header from "./component/Header";
-    import HomeBgImg from "./assets/MainBg.jpg";
+    import './assets/Style.css';
 
 function Home() {
     const navigate = useNavigate()
   return (
-    <MainBG>
+    <p class= "MainBG">
       <Header />,
-        <MainWrapper>
+        <p class= "MainWrapper">
           <h3>
           니집내집
           </h3>
-          <div>
-            당신을 위한 인테리어 플랫폼
-          </div>
-        </MainWrapper>
-    </MainBG>
+          <ul class="multi-layout">
+            <li>
+              <button type="button" onclick="location.href='/'">
+                인테리어 테스트
+              </button>
+              <button type="button" onclick="location.href='/'">
+                글 작성
+              </button>
+            </li>
+            <li>
+                <div>
+                당신을 위한 인테리어 플랫폼
+                </div>
+            </li>
+          </ul>
+        </p>
+    </p>
 
   );
 }
@@ -26,28 +37,3 @@ export default Home;
 
 
 
-const MainBG = styled.div`
-  height: 100vh;
-  width: 100vw;
-  background-image: url(${HomeBgImg});
-  background-repeat: no-repeat;
-  background-size: cover;
-  object-fit: cover;
-  display: grid;
-  place-items: center;
-`
-
-const MainWrapper = styled.div`
-  position: absolute;
-  min-height: 50vh;
-  min-width: 100vw;
-  display: grid;
-  place-items: center;
-  background-color: rgba(255, 255, 255, 0.8);
-  font-size: 2rem;
-  margin: 0, 1rem;
-  `
-  const h3 = styled.div`
-  font-size: 3rem;
-  margin: 0, 1rem;
-`
--- src/pages/component/Header.js
+++ src/pages/component/Header.js
@@ -1,22 +1,13 @@
-import styled from 'styled-components';
+import '../assets/Style.css';
 import React from "react";
 import { useNavigate } from "react-router-dom";
 
 function Header() {
 const navigate = useNavigate()
 return (
-    <Button> Logo</Button>
+    <button class="logo" type="button" onclick="location.href='/'"> Logo</button>
 );
 }
 
 export default Header;
 
-const Button = styled.button`
-  background: palevioletred;
-  font-size: 2em;
-  border: 2px solid palevioletred;
-  color: white;
-  margin: 0.2em;
-  padding: 0.25em 1em;
-  position: absolute; left: 30px; top: 30px;
-  `
