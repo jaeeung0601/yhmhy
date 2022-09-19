edbb4dba1bef9327f5c9788fd92ed9f0f63ef3ed
jaeeung0601 <choijaee9@gmail.com>
2022-09-19 15:41:56 +0900

    image 수정

--- src/routes/Notice.js
+++ src/routes/Notice.js
@@ -9,7 +9,8 @@ function App() {
     <div className="Noticeheader">
 
       <img className="NoticeheaderImage"
-      src="https://www.instagram.com/static/images/web/logged_out_wordmark.png/7a252de00b20.png"
+      src=" https://cdn-icons-png.flaticon.com/256/7207/7207414.png"
+      //src="https://www.instagram.com/static/images/web/logged_out_wordmark.png/7a252de00b20.png"
       alt=""/>
     </div>
     <h1>게시판z</h1>
