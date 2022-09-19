98cbea6bb9eb3baf196e3aeef3cc068352d1bdae
jaeeung0601 <choijaee9@gmail.com>
2022-09-19 14:59:40 +0900

    notice

---
+++ src/routes/Notice.js
@@ -0,0 +1,18 @@
+import React from "react";
+import '../App.css';
+import './assets/Style.css';
+
+function App() {
+  return (
+    <div className="App">
+    <div className="Noticeheader">
+
+      <img className="NoticeheaderImage"
+      src="https://img.icons8.com/emoji/48/000000/heart-suit.png"
+      alt=""/>
+    </div>
+    <h1>게시판</h1>
+    </div>
+  );
+}
+export default App;
