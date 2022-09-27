2b4a72f6887d78e3926b630b2100362f73777fea
sunY1129 <sla7821@naver.com>
2022-09-27 20:47:01 +0900

    KakaoShareButton

---
+++ src/Component/KakaoShareButton.js
@@ -0,0 +1,38 @@
+import React from "react";
+const {Kakao} = window;
+
+const KakaoShareButton = ({data}) =>{
+  const url = ''
+  //배포된 사이트 주소
+  const reaultUrl = window.location.href;
+  React.useEffect(()=>{
+    Kakao.cleanup()
+    Kakao.init("6a02ae0ce383262c8411b86feb03eb87")
+  },[])
+const shareKakao = () =>{
+  Kakao.Share.sendDefault({
+    objectType: 'feed',
+    content: {
+      title: '나에게 맞는 인테리어 스타일은?',
+      description: `당신과 어울리는 스타일은 "${data.name}" 입니다.`,
+      imageUrl: url + data.image,
+      link: {
+        mobileWebUrl: reaultUrl,
+        webUrl: reaultUrl,
+      },
+    },
+    buttons: [
+      {
+        title: '나도 테스트 하러가기',
+        link: {
+          mobileWebUrl: url,
+          webUrl: url,
+        },
+      },
+    ],
+  });
+}
+  return(<button onClick={shareKakao}>카카오톡 공유하기</button>)
+}
+
+export default KakaoShareButton
--- src/testpages/TestResult.js
+++ src/testpages/TestResult.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './Test.css'
 import { ResultData } from '../asset/data/resultdata'
 import { useNavigate, useSearchParams } from 'react-router-dom'
+import KakaoShareButton from '../Component/KakaoShareButton'
 
 const TestResult = () =>{
   const navigate = useNavigate()
@@ -17,10 +18,10 @@ const TestResult = () =>{
     <div className='testresult'>
       <h2>나에게 맞는 인테리어 스타일은?</h2>
       <h3>당신과 어울리는 스타일은 "{resultData.name}" 입니다.</h3>
-      <img src={resultData.image} width={500} height={350} alt='result img' />
-      <p>{resultData.desc}</p>
-      <button onClick={()=>navigate('/')}>테스트 다시하기</button>
-      <button>카카오톡 공유하기</button>
+      <img src={resultData.image} width={500} height={350} style={{marginTop:20}} alt='result img' />
+      <p style={{marginTop:20}}>{resultData.desc}</p>
+        <button onClick={()=>navigate('/')}>테스트 다시하기</button>
+        <KakaoShareButton data={resultData} />
     </div>
   )
 }
