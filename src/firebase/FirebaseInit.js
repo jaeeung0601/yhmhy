7c6fd8361b6135972593a259967ec6ae33cdccd6
KangWookLim <lakumwa99@gmail.com>
2022-09-28 19:25:11 +0900

    fix firebase

--- src/firebase/FirebaseInit.js
+++ src/firebase/FirebaseInit.js
@@ -3,15 +3,16 @@ import "firebase/compat/firestore";
 import "firebase/compat/auth";
 import "firebase/compat/storage";
 
-const firebaseApp = fb.initializeApp({
-    apiKey: "AIzaSyAF_9VzV4-1SDw1yGul_FL5PPCx_q2W8ws",
-    authDomain: "inst-f68e9.firebaseapp.com",
-    projectId: "inst-f68e9",
-    storageBucket: "inst-f68e9.appspot.com",
-    messagingSenderId: "825222020103",
-    appId: "1:825222020103:web:0ed6bc25f593e55793bea2",
-    measurementId: "G-84LJN9QL4K"
-});
+const firebaseConfig = {
+    apiKey: process.env.REACT_APP_API_KEY,
+    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+    projectId:  process.env.REACT_APP_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_MESSAGING_ID,
+    appId: process.env.REACT_APP_APP_ID
+}
+
+const firebaseApp = fb.initializeApp(firebaseConfig);
 
 const db = firebaseApp.firestore();
 const auth = fb.auth();
