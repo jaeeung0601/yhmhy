e87eb66d7fb9553a0c895740953b540636c3ee2d
KangWookLim <lakumwa99@gmail.com>
2022-09-29 20:02:42 +0900

    typo fix

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,4 +1,4 @@
-import { db, auth } from "../firebase/FirebaseInit";
+import { fb, auth } from "../firebase/FirebaseInit";
 import AuthForm from "./component/AuthForm";
 import "./assets/loginStyle.scss"
 import React from "react";
@@ -20,9 +20,9 @@ const Login = () => {
         } = event;
         let provider;
         if (name === "google"){
-            provider = new db.auth.GoogleAuthProvider();
+            provider = new fb.auth.GoogleAuthProvider();
         } else if (name === "github"){
-            provider = new db.auth.GithubAuthProvider();
+            provider = new fb.auth.GithubAuthProvider();
         }
         await auth.signInWithPopup(provider);
     };
