63b1e8d53ca427b2ef56c7a3d36237ef3bad8144
KangWookLim <lakumwa99@gmail.com>
2022-10-09 02:42:57 +0900

    fix Home scss

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,7 +2,15 @@
     import { useNavigate } from "react-router-dom";
     import Header from "./component/Header";
     import './assets/Style.scss';
-    import {Fade} from 'react-reveal';
+
+    import { Scrollbar, A11y, Autoplay } from 'swiper';
+    import { Swiper, SwiperSlide } from 'swiper/react';  
+    import 'swiper/scss';
+    import 'swiper/scss/navigation';
+    import 'swiper/scss/pagination';
+    import img1 from'../pages/assets/HomeImg/img1.jpg';
+    import img2 from'../pages/assets/HomeImg/img3.jpg';
+    import img3 from'../pages/assets/HomeImg/img2.jpg';
 
 function Home() {
     const navigate = useNavigate()
@@ -19,38 +27,50 @@ function Home() {
    navigate('/MktCommunity')
 }
   return (
-    <div className= "MainBG">
-      <Header />,
-        <div className= "MainWrapper">
-          <Fade clear>
-          <h3 className="MainHeadText">
-          니집내집
-          </h3>
-          </Fade>
-            <Fade claer delay={400}>
-            <div className="Maindiv">
-                당신을 위한 인테리어 플랫폼
-            </div>
-            </Fade>
-            <Fade clear delay={700}>
-            <div className="buttonA">
-              <button className="btn homebtn" type="button" onClick={handleClickButton}>
-                테스트 <br />시작하기
-              </button>
-            </div>
-            <div className="buttonB">
-              <button className="btn homebtn" type="button" onClick={handleClickButtonCM}>
-                커뮤니티
-              </button>
-            </div>
-            <div className="buttonD">
-              <button className="btn homebtn" type="button" onClick={handleClickButtonMkt}>
-                마켓
-              </button>
-            </div>
-              </Fade>
+    <div>
+      <Header />
+        <Swiper
+        modules={[Scrollbar, A11y, Autoplay]}
+        spaceBetween={100}
+        slidesPerView={1}
+        // navigation
+        // pagination={{ clickable: true }}
+        scrollbar={{ draggable: true }}
+        onSwiper={(swiper) => console.log(swiper)}
+        onSlideChange={() => console.log('slide change')}
+        autoplay={{ delay: 3000 }}	// 추가
+        speed={1000}
+        >
+          <SwiperSlide>
+          <img className="HomeImg" src={img1} />
+          </SwiperSlide>
+          <SwiperSlide>
+          <img className="HomeImg" src={img2} />
+          </SwiperSlide>
+          <SwiperSlide>
+          <img className="HomeImg" src={img3} />
+          </SwiperSlide>
 
+        </Swiper>
+      <div className="HomeContainer" >
+        <div className='NameContainer'>
+          니집내집
         </div>
+        <div className='TextContainer'>
+            "당신을 위한 인테리어 플렛폼"  
+        </div>
+        <div className='BtnContainer'>
+        <button className="AbsoluteBtn HomeBtn1" type="button" onClick={handleClickButton}>
+          커뮤니티
+        </button>
+        <button className="AbsoluteBtn HomeBtn2" type="button" onClick={handleClickButtonCM}>
+          테스트 <br />시작하기
+        </button>
+        <button className="AbsoluteBtn HomeBtn3" type="button" onClick={handleClickButtonMkt}>
+          마켓
+        </button>
+        </div>
+      </div>
     </div>
 
   );
