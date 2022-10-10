67494da86fa63eca9bade51b9181642ff7b35890
jaeeung0601 <choijaee9@gmail.com>
2022-10-10 20:08:32 +0900

    커밋

---
+++ README.md
@@ -0,0 +1,12 @@
+siteurl:
+https://bejewelled-platypus-3a3fd8.netlify.app/
+npm install -g create-react-app
+npm install -g firebase-tools 
+npm install --global yarn
+npm install —save react react-dom
+npm i bootstrap@5.2.1
+npm install reactstrap react react-dom
+npm install styled-components
+npm i --save @fortawesome/fontawesome-svg-core
+npm i @fortawesome/free-solid-svg-icons @fortawesome/free-regular-svg-icons @fortawesome/free-brands-svg-icons
+node_modules의 RevealBase.js 에서 componentWillReceiveProps를 componentDidUpdate로 변경
---
+++ src/pages/component/postMk/PostMk.js
@@ -0,0 +1,101 @@
+import React, { useState, useEffect } from "react";
+import "./PostMk.css";
+import Avatar from "@material-ui/core/Avatar";
+import { db, fb } from "../../../firebase/FirebaseInit";
+import { doc, deleteDoc, updateDoc, deleteField } from "firebase/firestore";
+
+
+function Post({ postId, user, username, caption, imageUrl }) {
+  const [comments, setComments] = useState([]);
+  const [comment, setComment] = useState("");
+  const onDelete = async () => {
+    const right = window.confirm("정말로 이 글을 삭제할까요?");
+    if (right) {
+      // unsubscribe = db
+      // .collection("posts")
+      // .doc(postId)
+      // .collection("comments")
+      // await fb.firestore.delete(doc(db, "comments", postId));
+      console.log(fb.firestore.postId);
+    }
+    };
+
+  useEffect(() => {
+    let unsubscribe;
+    if (postId) {
+      unsubscribe = db
+        .collection("marketposts")
+        .doc(postId)
+        .collection("comments")
+        .orderBy("timestamp", "asc")
+        .onSnapshot((snapshot) => {
+          setComments(snapshot.docs.map((doc) => doc.data()));
+        });
+    }
+    return () => {
+      unsubscribe();
+    };
+  }, [postId]);
+  const postComment = (e) => {
+    e.preventDefault();
+    db.collection("marketposts").doc(postId).collection("comments").add({
+      text: comment,
+      username: user.displayName,
+      timestamp: fb.firestore.FieldValue.serverTimestamp(),
+    });
+    setComment("");
+  };
+  return (
+    <div className="post">
+      <div className="post__header">
+        <Avatar
+          className="post__avatar"
+          alt={username}
+
+        />
+        <h3>{username}</h3>
+      </div>
+      <img className="post__image" src={imageUrl} alt="" />
+      <h4 className="post__text">
+        <strong>{username}</strong> {caption}
+      </h4>
+      {
+        <div className={comments.length > 0 ? "post__comments" : ""}>
+          {comments.map((comment, index) => (
+            <p doc={comment.username} key={index}>
+              <strong>{comment.username}</strong> {comment.text}
+            </p>
+          ))}
+        </div>
+      }
+      {user && (
+        <form className="comment__form">
+          <div className="comment__wrapper">
+            <input
+              className="comment__Input"
+              type="text"
+              placeholder="Add a comment..."
+              value={comment}
+              onChange={(e) => setComment(e.target.value)}
+            />
+            <button
+              className="comment__button text__button"
+              disabled={!comment}
+              onClick={postComment}
+              type="submit"
+            >
+              Post
+            </button>
+            <button
+              className="comment__button text__button" 
+              onClick={onDelete}>
+              Delete
+            </button>
+          </div>
+        </form>
+      )}
+    </div>
+  );
+}
+
+export default Post;
